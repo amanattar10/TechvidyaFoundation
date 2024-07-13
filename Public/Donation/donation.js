9ce6488534c5bae7e89519a64a309f1d36f67531
amanattar10 <amanattar678@gmail.com>
2024-07-13 16:42:03 +0530

    responsive update merged

---
+++ Public/Donation/donation.js
@@ -0,0 +1,6 @@
+var menu = document.getElementById("menu");
+var item = document.getElementById("nav")
+
+menu.onclick = function () {
+    item.style.display = item.style.display === "block" ? "none" : "block";
+}
